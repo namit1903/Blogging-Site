595e9b3061c3ca5ac9ee832fcd856546bffba19e
Thakur <mrhimachal1903@gmail.com>
2024-06-22 15:22:53 +0530

    title

--- backend/frontend/src/pages/Register.jsx
+++ backend/frontend/src/pages/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
   return (
     <>
       <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blog Market</Link></h1>
+    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Jot Me Down</Link></h1>
     <h3><Link to="/login">Login</Link></h3>
     </div>
     <div className="w-full flex justify-center items-center h-[80vh] ">
